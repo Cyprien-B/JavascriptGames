c056d6714fcfbeafaf6161d3af7a1aaaa9a6ffcc
Cyprien-B <cyprien.b@protonmail.com>
2022-04-29 23:25:58 +0200

    mise en place + switch

--- Breakout/app.js
+++ Breakout/app.js
@@ -2,6 +2,9 @@ const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
 
+const userStart = [230, 10];
+let currentPosition = userStart;
+
 // creat class block
 class Block {
     constructor(xAxis, yAxis) {
@@ -12,19 +15,59 @@ class Block {
     }
   }
 //ci dessous on construit tout les blocks
-  const blocks = [
-    new Block(10,270)
-  ]
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
+]
+
 function addBlocks() {
     for (let i = 0; i < blocks.length; i++) {
         const block = document.createElement('div')
         block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'  
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
         grid.appendChild(block)
-        block.style.left = blocks[i].bottomLeft[0] + 'px'
-        block.style.bottom ='100px'
         
     }
 
 }
 
 addBlocks()
+
+
+// add un user
+const user = document.createElement('div')
+user.classList.add("user")
+user.style.left= currentPosition[0]+ 'px'
+user.style.bottom= currentPosition[1]+ 'px'
+grid.appendChild(user)
+
+
+// bouger l'user
+function moveUser(e) {
+  switch (e.key) {
+    case 'Arrowleft':
+      currentPosition[0] -= 10
+      user.style.left= currentPosition[0]+ 'px'
+      
+      break;
+  
+    default:
+      break;
+  }
+}
+
+1.55
